dc795a9aa72bd566c764f908f9dff793b6a70f0d
Michael La Plante <michael@michaellaplante.com>
2013-08-24 11:45:16 -0400

    Initial Commit

---
+++ App1/LPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LPAppDelegate.h
+//  App1
+//
+//  Created by Michael La Plante on 8/24/13.
+//  Copyright (c) 2013 Michael La Plante. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ App1/LPViewController.h
@@ -0,0 +1,13 @@
+//
+//  LPViewController.h
+//  App1
+//
+//  Created by Michael La Plante on 8/24/13.
+//  Copyright (c) 2013 Michael La Plante. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LPViewController : UIViewController
+
+@end
